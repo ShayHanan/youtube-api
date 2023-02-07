c44ca18c4feb2708395af930a0e5eb7ec75cea26
shay hanan <shayhanan1@gmail.com>
2023-02-07 12:37:41 +0200

    ready to deploy

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/users.js"
-import videoRoutes from "./routes/videos.js"
-import commentRoutes from "./routes/comments.js"
-import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 const app = express();
 
@@ -14,27 +14,27 @@ dotenv.config();
 const connect = () => {
     mongoose.connect(process.env.MONGO).then(() => {
         console.log("connected to db");
-    }). catch(err => {throw err});
-}
+    }).catch(err => { throw err; });
+};
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/videos", videoRoutes);
-app.use("/api/comments", commentRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/videos", videoRoutes);
+app.use("/comments", commentRoutes);
 
 // Error handling
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Something went wrong";
     return res.status(status).json({
         success: false,
         status,
         message
-    })
-})
+    });
+});
 
-app.listen(8000, ()=> {
+app.listen(8000, () => {
     connect();
-    console.log("connected to server")
-})
+    console.log("connected to server");
+});
